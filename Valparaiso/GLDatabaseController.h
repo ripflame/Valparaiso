4ef1ac66c05bc4e765dbdb27b87b575c96d78e3b
Gilberto Leon <gilberto@ripfla.me>
2014-04-16 18:51:26 -0500

    SalesView implemented

---
+++ Valparaiso/GLDatabaseController.h
@@ -0,0 +1,17 @@
+//
+//  GLDatabaseController.h
+//  Valparaiso
+//
+//  Created by Gilberto Leon Enriquez on 16/4/14.
+//  Copyright (c) 2014 Ripflame. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GLDatabaseController : NSObject
+
++ (NSDictionary *) createSaleWithDate:(NSDate *)date quantity:(NSNumber *)quantity weight:(NSNumber *)weight andPrice:(NSNumber *)price;
+
++ (NSNumber *) getSalesTotalFromDay:(NSDate *)day;
+
+@end
